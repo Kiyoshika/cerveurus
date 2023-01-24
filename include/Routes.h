f9456576ac45b712ec930988952b38abdbd717e7
Kiyoshika <zachlweaver00@gmail.com>
2023-01-24 00:15:02 -0500

    implemented GET and POST request callbacks
    There is still some more work/cleanup to do but it's in a completely usable state now. Tested with cURL, postman and javascript fetch

--- include/Routes.h
+++ include/Routes.h
@@ -1,17 +1,45 @@
+#ifndef ROUTES_H
+#define ROUTES_H
+
 #include <stdlib.h>
 #include <string.h>
+
+// forward declaration
+struct ParameterArray;
 
 struct Route {
 	char* key;
 	char* value;
+	void* user_data;
+
+	char* (*get_callback)(
+			struct ParameterArray * params, 
+			void* user_data);
+	void (*post_callback)(
+			struct ParameterArray * params,
+			void* user_data,
+			char* request_body);
 
 	struct Route *left, *right;
 };
 
-struct Route * initRoute(char* key, char* value);
+struct Route * initRoute(
+		char* key, 
+		char* value,
+		void* user_data,
+		char* (*get_callback)(struct ParameterArray*, void*),
+		void (*post_callback)(struct ParameterArray*, void*, char*));
 
-void addRoute(struct Route ** root, char* key, char* value);
+void addRoute(
+		struct Route ** root, 
+		char* key, 
+		char* value,
+		void* user_data,
+		char* (*get_callback)(struct ParameterArray*, void*),
+		void (*post_callback)(struct ParameterArray*, void*, char*));
 
 struct Route * search(struct Route * root, char * key);
 
 void inorder(struct Route * root );
+
+#endif
--- src/main.c
+++ src/main.c
@@ -7,28 +7,68 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 #include "HTTP_Server.h"
 #include "Routes.h"
 #include "Response.h"
 #include "Parameters.h"
 
+// sample GET callback for /mystr (return the value of a local string)
+char* get_str(struct ParameterArray * params, void* user_data)
+{
+	(void)params;
+	return *(char**)user_data;
+}
+
+// sample POST callback for /mystr (set the value of a local string)
+void set_str(struct ParameterArray * params, void* user_data, char* request_body)
+{
+	(void)params;
+
+	// Our user_data is an address to a local char* variable.
+	// We cast to char** which holds the address and dereference + free before
+	// reallocating to the new string from the request_body
+	char** data = user_data;
+	free(*data);
+	*data = strdup(request_body);
+}
+
+// make these global so we can access them in the server interrupt
+struct ParameterArray * params;
+struct Route * route;
+
+// when user closes server with Ctrl+C we need to release the memory and close the socket
+void server_interrupt(int sig)
+{
+	signal(sig, SIG_IGN);
+	paramFree(params);
+	// TODO: add way to free memory held by route
+	exit(0);
+}
+
 int main() {
+	// enable Ctrl+C interrupt
+	signal(SIGINT, &server_interrupt);
+
 	// initiate HTTP_Server
 	HTTP_Server http_server;
 	init_server(&http_server, 6969);
 
-	int client_socket;
-	
 	// registering Routes
-	struct Route * route = initRoute("/", "index.html"); 
-	addRoute(&route, "/about", "about.html");
-	addRoute(&route, "/sth", "sth.html");
-	addRoute(&route, "/chicken", "chicken.html");
+	// TODO: split these into addTemplateRoute and addApiRoute since
+	// a template route will never have get/post callbacks (they only return HTML).
+	route = initRoute("/", "index.html", NULL, NULL, NULL); 
+	addRoute(&route, "/about", "about.html", NULL, NULL, NULL);
+	addRoute(&route, "/sth", "sth.html", NULL, NULL, NULL);
+	addRoute(&route, "/chicken", "chicken.html", NULL, NULL, NULL);
+
+	// TODO: might need to pass deallocators as well to free user data
+	char* my_str = strdup("hello there!");
+	addRoute(&route, "/mystr", NULL, &my_str, &get_str, &set_str);
 
 	// create dynamic parameters array
-	struct ParameterArray * params = paramInit(10);
-
+	params = paramInit(10);
 
 	printf("\n====================================\n");
 	printf("=========ALL AVAILABLE ROUTES========\n");
@@ -36,17 +76,21 @@ int main() {
 	inorder(route);
 
 	while (1) {
-		char client_msg[4096] = "";
+		char client_msg[4096] = {0};
+		char original_msg[4096] = {0};
 
-		client_socket = accept(http_server.socket, NULL, NULL);
+		int client_socket = accept(http_server.socket, NULL, NULL);
 
-		read(client_socket, client_msg, 4095);
+		size_t n_bytes_read = read(client_socket, client_msg, 4095);
+		client_msg[n_bytes_read] = '\0';
+		memcpy(original_msg, client_msg, n_bytes_read + 1);
 		printf("%s\n", client_msg);
 
 		// parsing client socket header to get HTTP method, route
 		char *method = "";
 		char *urlRoute = "";
 
+		// TODO: cleanup this parsing so we can also fetch Content-Length (or other headers in the future)
 		char *client_http_header = strtok(client_msg, "\n");
 			
 		printf("\n\n%s\n\n", client_http_header);
@@ -85,34 +129,65 @@ int main() {
 			// do something with parameters and can clear them to re-use for the next
 			// request
 			paramClear(params);
-		}
 
-
-		char template[100] = "";
-		
-		if (strstr(urlRoute, "/static/") != NULL) {
-			//strcat(template, urlRoute+1);
-			strcat(template, "static/index.css");
-			http_set_status_code(&http_server, OK);
-		}else {
-			struct Route * destination = search(route, urlRoute);
-			strcat(template, "templates/");
-
-			if (destination == NULL) {
-				strcat(template, "404.html");
-				http_set_status_code(&http_server, NOT_FOUND);
-			}else {
-				strcat(template, destination->value);
+			// TODO: abstract the response/set/set/send portion (e.g., http_send_file, http_send_get, http_send_post, etc.)
+			char template[100] = "";
+			
+			// TODO: fix rendering of static files (currently broken..oops)
+			if (strstr(urlRoute, "/static/") != NULL)
+			{
+				strcat(template, "static/index.css");
 				http_set_status_code(&http_server, OK);
+				send(client_socket, template, strlen(template), 0);
+				continue;
+			}
+
+			struct Route * destination = search(route, urlRoute);
+
+			if (destination == NULL)
+			{
+				char* response_data = render_static_file("templates/404.html");
+				http_set_status_code(&http_server, NOT_FOUND);
+				http_set_response_body(&http_server, response_data);
+				send(client_socket, http_server.response, strlen(http_server.response), 0);
+				continue;
+			}
+
+			if (destination->value)
+			{
+				strcat(template, "templates/");
+				strcat(template, destination->value);
+				char* response_data = render_static_file(template);
+				http_set_status_code(&http_server, OK);
+				http_set_response_body(&http_server, response_data);
+				send(client_socket, http_server.response, strlen(http_server.response), 0);
+				free(response_data);
+			}
+			else
+			{
+				if (strcmp(method, "GET") == 0)
+				{
+					char* response_data = destination->get_callback(params, destination->user_data);
+					http_set_status_code(&http_server, OK);
+					http_set_response_body(&http_server, response_data);
+					send(client_socket, http_server.response, strlen(http_server.response), 0);
+				}
+				// TODO: there's a really annoying issue with sending POST requests via cURL/postman
+				// where you have to refresh the site 1-2 times before it goes through.
+				// Currently have no idea why this happens...
+				// Doing the requests using fetch() in javascript are instantaneous
+				else if (strcmp(method, "POST") == 0)
+				{
+					destination->post_callback(params, destination->user_data, original_msg);
+					http_set_status_code(&http_server, OK);
+					http_set_response_body(&http_server, "");
+					send(client_socket, http_server.response, strlen(http_server.response), 0);
+				}
 			}
 		}
 
-		char * response_data = render_static_file(template);
-		http_set_response_body(&http_server, response_data);
-
-		send(client_socket, http_server.response, sizeof(http_server.response), 0);
 		close(client_socket);
-		free(response_data);
 	}
+
 	return 0;
 }
